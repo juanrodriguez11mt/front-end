1013dee32ae0eebd8c3eb0812f348afe3864fb9b
juanrodriguez11mt <juan.rodriguez11.mt@correo.usa.edu.co>
2021-10-05 11:33:14 -0500

    Detalles, actualizar

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,8 +1,12 @@
 var ruta = "https://g55a3c31906132b-basedatosrenta.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/car/car";
 
 function cargarLista() {
-    $('#cars-table tbody').empty();
     $('#mensaje').text("Cargando datos ...");
+    listaCarros();
+}
+
+function listaCarros() {
+    $('#cars-table tbody').empty();
     $.ajax({    
         url : ruta,
         data : { },
@@ -10,8 +14,8 @@ function cargarLista() {
         dataType : 'json',
         crossDomain: true,
     
-        success : function(json) {
-            var items = json.items;
+        success : function(response) {
+            var items = response.items;
             for (var i=0; i<items.length; i++)
             {
                 var item = items[i]
@@ -21,8 +25,92 @@ function cargarLista() {
                                 '<td class="col">' + item.brand + '</td>' +
                                 '<td class="col">' + item.model + '</td>' +
                                 '<td class="col">' + item.category_id + '</td>' +
+                                '<td class="col"><a href="detalle.html?id=' + item.id + '">Detalle</td>' +
                             '</tr>');
             };
+        },
+        error : function(xhr, status) {
+            console.log('ha sucedido un problema');
+        },
+        complete : function(xhr, status) {
+            $('#mensaje').empty();
+            console.log('Petición realizada');
+        }
+    });
+}
+
+function cargarDetalles() {
+    $('#mensaje').text("Cargando datos ...");
+    var id = obtenerParametroId();
+    $.ajax({    
+        url : ruta + "/" + id,
+        data : { },
+        type : 'GET',
+        dataType : 'json',
+        crossDomain: true,
+        
+        success : function(response) {
+            var items = response.items;
+            if (items.length < 1) {
+                alert('No fue posible cargar los detalles del carro')
+            }
+            else {
+                var item = items[0];
+                $("#car-form").css("display", "block");
+                $('#car-id').val(item.id);
+                $('#car-brand').val(item.brand);
+                $('#car-model').val(item.model);
+                $('#car-category').val(item.category_id);
+                $('#car-brand').prop("disabled", false)
+                $('#car-model').prop("disabled", false)
+                $('#car-category').prop("disabled", false)
+            }
+        },
+        error : function(xhr, status) {
+            console.log('ha sucedido un problema');
+        },
+        complete : function(xhr, status) {
+            $('#mensaje').empty();
+            console.log('Petición realizada');
+        }
+    });
+}
+
+function obtenerParametroId() {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === 'id') {
+            return sParameterName[1];
+        }
+    }
+    return undefined;
+};
+
+function actualizar() {
+    var body = { 
+        id: $("#car-id").val(),
+        brand: $("#car-brand").val(),
+        model: $("#car-model").val(),
+        category_id: $("#car-category").val(),
+    }
+    
+    $.ajax({    
+        url : ruta,
+        data : JSON.stringify(body),
+        type : 'PUT',
+        contentType: 'application/json',
+    
+        success : function(response, status) {
+            alert('Datos actualizados');
+            $('#car-brand').prop("disabled", true)
+            $('#car-model').prop("disabled", true)
+            $('#car-category').prop("disabled", true)
         },
         error : function(xhr, status) {
             console.log('ha sucedido un problema');
